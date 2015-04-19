d0d93586554978464f31d1038adf5e0815efed60
Arjun Passi <lakhan211@yahoo.com>
2015-04-19 19:40:28 -0400

    Added hashmap of locations

---
+++ app/src/main/java/com/cs3714/apassi/hokiehelper/LocationMap.java
@@ -0,0 +1,76 @@
+package com.cs3714.apassi.hokiehelper;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.HashMap;
+
+/**
+ * Created by apassi on 4/19/15.
+ */
+public class LocationMap {
+
+    public static final HashMap<String, LatLng> buildings = new HashMap<String, LatLng>();
+
+    static {
+
+        buildings.put("AA", new LatLng( (37.2286 ),  (-80.41629 )));
+
+        buildings.put("MCB", new LatLng( (37.230311 ),  (-80.421771 )));
+        buildings.put("DER", new LatLng( (37.229064 ),  (-80.425504 )));
+        buildings.put("HAHN N", new LatLng( (37.228552 ),  (-80.426685 )));
+        buildings.put("HAHN S", new LatLng( (37.227817 ),  (-80.425686 )));
+        buildings.put("TORG", new LatLng( (37.229833 ),  (-80.420398 )));
+
+        buildings.put("RAND", new LatLng( (37.230499 ),  (-80.42338 )));
+        buildings.put("HOLD", new LatLng( (37.230132 ),  (-80.422069 )));
+        buildings.put("NOR", new LatLng( (37.229483 ),  (-80.423196 )));
+
+        buildings.put("HAN", new LatLng( (37.230098 ),  (-80.424044 )));
+        buildings.put("WHIT", new LatLng( (37.230696 ),  (-80.424719 )));
+        buildings.put("PAT", new LatLng( (37.229235 ),  (-80.422145 )));
+        buildings.put("CO", new LatLng( (37.230004 ),  (-80.424943 )));
+
+        buildings.put("DAV", new LatLng( (37.227031 ),  (-80.425093 )));
+        buildings.put("ROB", new LatLng( (37.22821 ),  (-80.425115 )));
+        buildings.put("PAM", new LatLng( (37.228543 ),  (-80.424203 )));
+
+        buildings.put("SURGE", new LatLng( (37.232985 ),  (-80.423183 )));
+        buildings.put("MAJWM", new LatLng( (37.227894 ),  (-80.424342 )));
+        buildings.put("SQUIR", new LatLng( (37.22956 ),  (-80.417497 )));
+
+        buildings.put("WLH", new LatLng( (37.230905 ),  (-80.422256 )));
+        buildings.put("EMPOR", new LatLng( (37.228552 ),  (-80.422972 )));
+
+        buildings.put("WAL", new LatLng( (37.22296 ),  (-80.424187 )));
+        buildings.put("LITRV", new LatLng( (37.221606 ),  (-80.423771 )));
+        buildings.put("ENGEL", new LatLng( (37.223793 ),  (-80.423249 )));
+        buildings.put("GYM", new LatLng( (37.223793 ),  (-80.423249 )));
+
+        buildings.put("FEM", new LatLng( (37.231344 ),  (-80.421242 )));
+        buildings.put("AGNEW", new LatLng( (37.224754 ),  (-80.424187 )));
+        buildings.put("BURCH", new LatLng( (37.229615 ),  (-80.424193 )));
+        buildings.put("BUR", new LatLng( (37.228991 ),  (-80.423657 )));
+
+        buildings.put("DURHM", new LatLng( (37.231757 ),  (-80.423705 )));
+        buildings.put("HEND", new LatLng( (37.230599 ),  (-80.416681 )));
+        buildings.put("HUTCH", new LatLng( (37.225531 ),  (-80.423164 )));
+        buildings.put("JCH", new LatLng( (37.223868 ),  (-80.422744 )));
+
+        buildings.put("LANE", new LatLng( (37.230832 ),  (-80.419677 )));
+        buildings.put("LATH", new LatLng( (37.224575 ),  (-80.422443 )));
+        buildings.put("LIBR", new LatLng( (37.228819 ),  (-80.419606 )));
+        buildings.put("LIBR", new LatLng( (37.2306 ),  (-80.420874 )));
+
+        buildings.put("PRICE", new LatLng( (37.22579 ),  (-80.424117 )));
+        buildings.put("SANDY", new LatLng( (37.225814 ),  (-80.42348 )));
+        buildings.put("SAUND", new LatLng( (37.224875 ),  (-80.424483 )));
+        buildings.put("SEITZ", new LatLng( (37.22456 ),  (-80.423546 )));
+
+        buildings.put("SHANK", new LatLng( (37.231551 ),  (-80.420117 )));
+        buildings.put("SHULTZ", new LatLng( (37.231801 ),  (-80.418714 )));
+        buildings.put("SMYTH", new LatLng( (37.224932 ),  (-80.422911 )));
+        buildings.put("SQUIR", new LatLng( (37.229455 ),  (-80.417589 )));
+
+        buildings.put("WMS", new LatLng( (37.227824 ),  (-80.424234 )));
+    }
+}
