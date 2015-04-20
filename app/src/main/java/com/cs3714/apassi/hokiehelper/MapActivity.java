3c9f7c535cffa98204c1f3289324ef7c9d26b3cd
Arjun Passi <lakhan211@yahoo.com>
2015-04-20 13:28:05 -0400

    getting directions from google maps

--- app/src/main/java/com/cs3714/apassi/hokiehelper/MapActivity.java
+++ app/src/main/java/com/cs3714/apassi/hokiehelper/MapActivity.java
@@ -1,5 +1,8 @@
 package com.cs3714.apassi.hokiehelper;
 
+import android.graphics.Color;
+import android.os.StrictMode;
+import android.provider.DocumentsContract;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -8,23 +11,69 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import org.w3c.dom.Document;
 
 public class MapActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    GMapV2Direction md;
+
+    LatLng fromPosition = LocationMap.buildings.get("SURGE");
+    LatLng toPosition = LocationMap.buildings.get("LITRV");
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
         setUpMapIfNeeded();
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        md = new GMapV2Direction();
+        mMap = ((SupportMapFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
+
+        LatLng coordinates = new LatLng(13.685400079263206, 100.537133384495975);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(fromPosition, 16));
+
+        mMap.addMarker(new MarkerOptions().position(fromPosition).title("Start"));
+        mMap.addMarker(new MarkerOptions().position(toPosition).title("End"));
+
+        Document doc = md.getDocument(fromPosition, toPosition, GMapV2Direction.MODE_WALKING);
+        int duration = md.getDurationValue(doc);
+        String distance = md.getDistanceText(doc);
+        String start_address = md.getStartAddress(doc);
+        String copy_right = md.getCopyRights(doc);
+
+        ArrayList<LatLng> directionPoint = md.getDirection(doc);
+        PolylineOptions rectLine = new PolylineOptions().width(3).color(Color.RED);
+
+        for(int i = 0 ; i < directionPoint.size() ; i++) {
+            rectLine.add(directionPoint.get(i));
+        }
+
+        mMap.addPolyline(rectLine);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setUpMapIfNeeded();
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_map);
+//        setUpMapIfNeeded();
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        setUpMapIfNeeded();
+//    }
 
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
