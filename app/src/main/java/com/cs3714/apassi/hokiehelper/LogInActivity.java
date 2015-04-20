b51b88e25f17338d2dd1f0694194f7c2d4f4e236
Arjun Passi <lakhan211@yahoo.com>
2015-04-20 13:28:05 -0400

    getting directions from google maps

--- app/src/main/java/com/cs3714/apassi/hokiehelper/LogInActivity.java
+++ app/src/main/java/com/cs3714/apassi/hokiehelper/LogInActivity.java
@@ -1,17 +1,32 @@
 package com.cs3714.apassi.hokiehelper;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 
 public class LogInActivity extends ActionBarActivity {
+
+    private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setUpLogInButton();
     }
 
 
@@ -20,6 +35,29 @@ public class LogInActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_log_in, menu);
         return true;
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            Log.d("LogIn", data.getData().toString());
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void setUpLogInButton() {
+        Button button = (Button) findViewById(R.id.logInButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+//                        Uri.parse("http://maps.google.com/maps?saddr=37.2286,-80.41629&daddr=37.221606,-80.423771"));
+//                startActivityForResult(intent, REQUEST_CODE);
+
+                Intent intent = new Intent(LogInActivity.this , MapActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
