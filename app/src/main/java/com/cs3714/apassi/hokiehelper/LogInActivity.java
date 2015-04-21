6d1bfaa5dc281cd7727506f55ff6da6128516752
Samuel Bahr <sfbahr@gmail.com>
2015-04-21 13:44:43 -0400

    Add CAS login
    It will print a toast saying whether the login failed or succeeded.

--- app/src/main/java/com/cs3714/apassi/hokiehelper/LogInActivity.java
+++ app/src/main/java/com/cs3714/apassi/hokiehelper/LogInActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,20 +12,30 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.cs3714.apassi.hokiehelper.vtaccess.Cas;
+import com.cs3714.apassi.hokiehelper.vtaccess.WrongLoginException;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 
 public class LogInActivity extends ActionBarActivity {
-
     private static final int REQUEST_CODE = 1;
+    private EditText pidText;
+    private EditText passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        pidText = (EditText) findViewById(R.id.userNameEditText);
+        passwordText = (EditText) findViewById(R.id.passwordEditText);
 
         setUpLogInButton();
     }
@@ -45,8 +56,14 @@ public class LogInActivity extends ActionBarActivity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+    public void onClickLogin(View view) throws WrongLoginException {
+        new LoginTask().execute(pidText.getText().toString(),
+                passwordText.getText().toString(),
+                new File(getCacheDir(), "ssl").toString());
+    }
+
     private void setUpLogInButton() {
-        Button button = (Button) findViewById(R.id.logInButton);
+        Button button = (Button) findViewById(R.id.toScheduleButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,5 +90,28 @@ public class LogInActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class LoginTask extends AsyncTask<String, Integer, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... params) {
+            boolean success = false;
+            try {
+                success = Cas.login(params[0], params[1], params[2]);
+            } catch (WrongLoginException e) {
+                e.printStackTrace();
+                return false;
+            }
+            return success;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            if (success) {
+                Toast.makeText(LogInActivity.this, "Login succeeded", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(LogInActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
